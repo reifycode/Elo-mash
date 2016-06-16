981ec3a1eace4cb544ae4b0b709ea384111944a7
Pulkit Agarwal <thepulkitagarwal@gmail.com>
2016-06-16 14:49:57 +0530

    Create Elo module

--- elo.js
+++ elo.js
@@ -1,13 +1,18 @@
-var photolist = [
-	{
-		name: "abc.jpg"
-	},
-	{
-		name: "def.jpg"
-	}
-];
+var k_factor = 32;
 
-function expectedScore(Ra, Rb) {
-	return 1 / (1 + Math.pow(10, (Ra - Rb)/400));
+exports.defineKFactor = function(k) {
+	k_factor = k;
 }
 
+exports.expectedScore = (Ra, Rb) => 1 / (1 + Math.pow(10, (Ra - Rb)/400));
+
+exports.competition = function(photoA, photoB, scoreA) { 
+	// scoreA = If a wins: 1, loss: 0
+	var scoreB = 1 - scoreA;
+
+	Ra = photoA.rating + k_factor * (scoreA - expectedScore(photoA.rating, photoB.rating));
+	Rb = photoB.rating + k_factor * (scoreB - expectedScore(photoB.rating, photoA.rating));
+
+	photoA.rating = Ra;
+	photoB.rating = Rb;
+}
